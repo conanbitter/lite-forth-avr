1a02e942ec4a2ad450f01d422e1843852641bf8a
Conan Bitter <conan.bitter@gmail.com>
2026-02-07 18:15:53 +0300

    Implement static NEXT macro

--- main.asm
+++ main.asm
@@ -4,40 +4,38 @@
 
 			.include "m328Pdef.inc"
 
+; ========== DEFINES =========
+
+			.def	codePtr  = r22
+			.def	codePtrH = r23
+
 ; ========== MACROS ==========
+
+		.macro NEXT_STATIC
+			movw	ZL, codePtr			; Z = codePtr
+			subi	codePtr, low(-2)	; codePtr += 2
+			sbci	codePtrH, high(-2)
+			lpm		r16, z+				; Z = *Z { temp = *Z
+			lpm		r17, z
+			movw	ZL, r16				; Z = temp }
+			ijmp						; goto Z
+		.endm
 
 ; =========== RAM ============
 
 ; ========== CODE ============
 
-			.def	mask    = r16
-			.def	ledR    = r17
-			.def	oLoopR  = r18
-			.def	iLoopRL = r24
-			.def	iLoopRH = r25
-
-			.equ	oVal = 71
-			.equ	iVal = 28168
-
 			.cseg
 			.org	0x00
 
-			clr		ledR
-			ldi		mask, (1<<PINB5)
-			out		DDRB, mask
+test1:		ldi		codePtr, low(test2*2)
+			ldi		codePtrH, high(test2*2)
+			nop
+test3:		nop
+			nop
+			nop
+			NEXT_STATIC
+			nop
 
-start:		eor		ledR, mask
-			out		PORTB, ledR
-
-			ldi		oLoopR, oVal
-
-oLoop:		ldi		iLoopRL, low(iVal)
-			ldi		iLoopRH, high(iVal)
-
-iLoop:		sbiw	iLoopRL, 1
-			brne	iLoop
-
-			dec		oLoopR
-			brne	oLoop
-
-			rjmp	start
+			.org	0x7F
+test2:		.dw		test3
