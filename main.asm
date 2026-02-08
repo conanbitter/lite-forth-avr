e141b018bfa578f0dcd89a1fb0e13f9985bac6f3
Conan Bitter <conan.bitter@gmail.com>
2026-02-08 11:20:10 +0300

    Improve and fix NEXT subroutine

--- main.asm
+++ main.asm
@@ -6,36 +6,55 @@
 
 ; ========== DEFINES =========
 
-			.def	codePtr  = r22
-			.def	codePtrH = r23
+			.def	wordIsCore = r2
+			.def	codePtr    = r22
+			.def	codePtrH   = r23
 
 ; ========== MACROS ==========
 
-		.macro NEXT_STATIC
-			movw	ZL, codePtr			; Z = codePtr
-			subi	codePtr, low(-2)	; codePtr += 2
-			sbci	codePtrH, high(-2)
-			lpm		r16, z+				; Z = *Z { temp = *Z
-			lpm		r17, z
-			movw	ZL, r16				; Z = temp }
-			ijmp						; goto Z
-		.endm
-
 ; =========== RAM ============
+
+			.dseg
+test4:		.byte 2
 
 ; ========== CODE ============
 
 			.cseg
 			.org	0x00
 
-test1:		ldi		codePtr, low(test2*2)
-			ldi		codePtrH, high(test2*2)
+test1:		nop
+;			ldi		codePtr, low(test2*2)
+;			ldi		codePtrH, high(test2*2)
+;			ldi		r16, 1
+			ldi		codePtr, low(test4)
+			ldi		codePtrH, high(test4)
+			ldi		r16, 0
+			mov		wordIsCore, r16
+			ldi		r16, low(test5)
+			sts		test4, r16
+			ldi		r16, high(test5)
+			sts		test4+1, r16
 			nop
 test3:		nop
 			nop
+test5:		nop
 			nop
-			NEXT_STATIC
 			nop
+			rjmp	NEXT
+			nop
+
+NEXT:		movw	ZL, codePtr			; Z = codePtr
+			subi	codePtr, low(-2)	; codePtr += 2
+			sbci	codePtrH, high(-2)
+			tst		wordIsCore			; Z = *Z {
+			brne	NEXT_ELSE
+			ld		r16, Z+				; temp = *Z  (from RAM)
+			ld		r17, Z
+			rjmp	NEXT_ENDIF
+NEXT_ELSE:	lpm		r16, Z+				; temp = *Z  (from Flash)
+			lpm		r17, Z
+NEXT_ENDIF:	movw	ZL, r16				; Z = temp }
+			ijmp						; goto Z
 
 			.org	0x7F
 test2:		.dw		test3
